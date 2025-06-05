67f05afa6f5b8690ba756bb008ba8041e8804667
sofacadeiramesa1234 <sofacadeiramesa1234@gmail.com>
2025-06-05 19:19:12 +0000

    commit

--- index.js
+++
@@ -1,4 +0,0 @@
-// Faz a página rolar suavemente até uma seção com o ID indicado
-function scrollToSection(id) {
-  document.getElementById(id).scrollIntoView({ behavior: "smooth" });
-}
---
+++ js/images.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const abrirMapaBtn = document.getElementById("abrirMapa");
+  const fecharMapaBtn = document.getElementById("fecharMapa");
+  const mapModal = document.getElementById("mapModal");
+
+  abrirMapaBtn.addEventListener("click", () => {
+    mapModal.style.display = "block";
+  });
+
+  fecharMapaBtn.addEventListener("click", () => {
+    mapModal.style.display = "none";
+  });
+});
